34329c1d2888bb54ab7754b5ea1c79b459e28b9e
Egle Sigler <ushnishtha@hotmail.com>
2018-07-21 20:18:51 -0500

    adding more solidfire calls

--- solidfire_common.py
+++ solidfire_common.py
@@ -14,16 +14,24 @@ def get_solid_fire():
 
 def get_volumes():
     sf = get_solid_fire()
-    vls = sf.list_volumes_for_account()
-    for vl in vls:
-        print vl
+    vls = sf.list_volumes()
+    return vls
+
+
+def get_volume_by_volume_name(name):
+    vls = get_volumes()
+    for vl in vls.volumes:
+        if vl.name.endswith(name):
+            print "Found SolidFire volume for Cinder ID " + name
+            print "SolidFire volume name " + vl.name + " SolidFire volume ID: " + vl.volume_id
+            return vl.volume_id
+    print "No SolidFire volume found for Cinder ID " + name
+    return None
 
 
 def main():
-    get_volumes()
-
+    get_volume_by_volume_name('81f16643-a50c-43bf-84e3-d67ba35cd222')
+    
 
 if __name__== "__main__":
   main()
-
-
